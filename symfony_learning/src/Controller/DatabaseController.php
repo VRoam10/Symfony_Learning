ffd5f750467d16ae995d9ec476118b0588393307
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2024-10-14 14:20:44 +0200

    Connexion to database

--- symfony_learning/src/Controller/DatabaseController.php
+++ symfony_learning/src/Controller/DatabaseController.php
@@ -3,9 +3,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use Doctrine\DBAL\Connection;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\DBAL\Connection;
 
 class DatabaseController
 {
@@ -19,21 +21,39 @@ class DatabaseController
     /**
      * @Route("/user-info", name="user_info", methods={"GET"})
      */
-    public function showUserInfo(): Response
-    {
-        // Example query to fetch user information from the "users" table
-        $query = 'SELECT * FROM users';
-        $users = $this->connection->executeQuery($query)->fetchAllAssociative();
+    // public function showUserInfo(): Response
+    // {
+    //     // Example query to fetch user information from the "users" table
+    //     $query = 'SELECT * FROM users';
+    //     $users = $this->connection->executeQuery($query)->fetchAllAssociative();
 
-        // Display user information
-        $response = '<h1>User Information</h1>';
-        foreach ($users as $user) {
-            $response .= '<p>ID: '.$user['id'].'</p>';
-            $response .= '<p>Name: '.$user['name'].'</p>';
-            $response .= '<p>Email: '.$user['email'].'</p>';
-            $response .= '<hr>';
+    //     // Display user information
+    //     $response = '<h1>User Information</h1>';
+    //     foreach ($users as $user) {
+    //         $response .= '<p>ID: '.$user['id'].'</p>';
+    //         $response .= '<p>Name: '.$user['name'].'</p>';
+    //         $response .= '<p>Email: '.$user['email'].'</p>';
+    //         $response .= '<hr>';
+    //     }
+
+    //     return new Response($response);
+    // }
+    
+    #[Route('/Test/{id}', name: 'product_show')]
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $product = $doctrine->getRepository(Users::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
         }
 
-        return new Response($response);
+        return new Response('Check out this great product: '.$product->getNom());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
     }
 }
---
+++ symfony_learning/src/Entity/Users.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+class Users
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?int $telephone = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $id_entreprise = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(int $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getIdEntreprise(): ?int
+    {
+        return $this->id_entreprise;
+    }
+
+    public function setIdEntreprise(?int $id_entreprise): static
+    {
+        $this->id_entreprise = $id_entreprise;
+
+        return $this;
+    }
+}
